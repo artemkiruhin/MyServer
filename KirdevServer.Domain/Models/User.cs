431574211ead6256d325a90e6907ea8b39545fb0
ArtemPC <temkaunik@gmail.com>
2024-09-20 16:59:52 +0300

    User Create method

--- KirdevServer.Domain/Models/User.cs
+++ KirdevServer.Domain/Models/User.cs
@@ -8,5 +8,20 @@
         public string PasswordHash { get; set; } = string.Empty;
         public DateOnly Birthday { get; set; } = DateOnly.MinValue;
         public DateTime Registrered { get; set; } = DateTime.Now;
+
+        private User(Guid id, string username, string email, string passwordHash, DateOnly birthday)
+        {
+            Id = id;
+            Username = username;
+            Email = email;
+            PasswordHash = passwordHash;
+            Birthday = birthday;
+            Registered = DateTime.Now;
+        }
+
+        public static User Create(Guid id, string username, string email, string passwordHash, DateOnly birthday)
+        {
+            return new User(id, username, email, passwordHash, birthday);
+        }
     }
 }
--- KirdevServer.Domain/obj/Debug/net8.0/KirdevServer.Domain.AssemblyInfo.cs
+++ KirdevServer.Domain/obj/Debug/net8.0/KirdevServer.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KirdevServer.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71a0628bcf1b28a538c404cd3b2ac35c8fce66dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f0140dbf883e5a1064fc1a1b96b93f02d9b66b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("KirdevServer.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KirdevServer.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
