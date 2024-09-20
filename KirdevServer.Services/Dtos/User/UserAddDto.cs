fb622de3579bdece3ad21d41727d0dcae6c40f9f
ArtemPC <temkaunik@gmail.com>
2024-09-20 16:55:39 +0300

    UserAddDto created

---
+++ KirdevServer.Services/Dtos/User/UserAddDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KirdevServer.Services.Dtos.User
+{
+    public class UserAddDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateOnly Birthday { get; set; } = DateOnly.MinValue;
+        public DateTime Registrered { get; set; } = DateTime.Now;
+    }
+}
+}
--- KirdevServer.Services/obj/Debug/net8.0/KirdevServer.Services.AssemblyInfo.cs
+++ KirdevServer.Services/obj/Debug/net8.0/KirdevServer.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KirdevServer.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7c03668f17f4ff5c53296c619afc69c0569f7f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6418c92e674258ac747ab71f5408f1c08cd6f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("KirdevServer.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KirdevServer.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
