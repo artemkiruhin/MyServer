456ed3640b5cc5ddd89296e09a5eef8353098fb9
ArtemPC <temkaunik@gmail.com>
2024-09-20 17:04:21 +0300

    UserAddDto fixed

--- KirdevServer.Services/Dtos/User/UserAddDto.cs
+++ KirdevServer.Services/Dtos/User/UserAddDto.cs
@@ -4,7 +4,7 @@
     {
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
         public DateOnly Birthday { get; set; } = DateOnly.MinValue;
         public DateTime Registrered { get; set; } = DateTime.Now;
     }
