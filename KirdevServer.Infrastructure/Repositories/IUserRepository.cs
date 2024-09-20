2e717be8bce3c02551d7ab3cd9c2904dc6ab74eb
ArtemPC <temkaunik@gmail.com>
2024-09-20 16:33:37 +0300

    IUserRepository created

---
+++ KirdevServer.Infrastructure/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using KirdevServer.Domain.Models;
+using KirdevServer.Infrastructure.Repositories.Base;
+
+namespace KirdevServer.Infrastructure.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> GetByUsernameAndPasswordAsync (string username, string password);
+    }
+}
--- KirdevServer.Infrastructure/obj/Debug/net8.0/KirdevServer.Infrastructure.AssemblyInfo.cs
+++ KirdevServer.Infrastructure/obj/Debug/net8.0/KirdevServer.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KirdevServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b31145ed5453a482661f5f2d5f8f6392db68b388")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf17e83f3f615d738b73caaa92e80448f1404539")]
 [assembly: System.Reflection.AssemblyProductAttribute("KirdevServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KirdevServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
