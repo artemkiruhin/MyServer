79ef312a2d1c145cd2e64d9aa01da38d8da45766
ArtemPC <temkaunik@gmail.com>
2024-09-20 16:55:53 +0300

    UserAddDto optimised

--- KirdevServer.Services/Dtos/User/UserAddDto.cs
+++ KirdevServer.Services/Dtos/User/UserAddDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KirdevServer.Services.Dtos.User
+﻿namespace KirdevServer.Services.Dtos.User
 {
     public class UserAddDto
     {
